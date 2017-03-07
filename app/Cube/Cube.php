debbada902fc0348dc1df6ea729f18a6a8455115
Felipe Angel <fangel@hugeinc.com>
2017-03-06 22:36:52 -0500

    Write logic for update query

--- app/Cube/Cube.php
+++ app/Cube/Cube.php
@@ -11,9 +11,17 @@ class Cube
      */
     protected $matrix;
 
+    /**
+     * Current size of the cube.
+     *
+     * @var multidimensional array
+     */
+    protected $size;
+
     public function __construct ()
     {
         $this->matrix = array();
+        $this->size = 0;
     }
 
     /**
@@ -26,12 +34,38 @@ class Cube
     public function init ($size)
     {
         $this->matrix = array();
-        for ($i = 0; $i < $size; $i++) {
-            for ($j = 0; $j < $size; $j++) {
-                for ($k = 0; $k < $size; $k++) {
-                    $this->matrix[$i][$j][$k] = 0;
+        for ($x = 0; $x < $size; $x++) {
+            for ($y = 0; $y < $size; $y++) {
+                for ($z = 0; $z < $size; $z++) {
+                    $this->matrix[$x][$y][$z] = 0;
                 }
             }
         }
+        $this->size = $size;
+    }
+
+    /**
+     * Set a value in a cube's position.
+     *
+     * @param int    $x     X coordinate
+     * @param int    $y     Y coordinate
+     * @param int    $z     Z coordinate
+     * @param int    $value Value to store
+     *
+     * @return void
+     */
+    public function update ($x, $y, $z, $value)
+    {
+        $this->matrix[$x][$y][$z] = $value;
+    }
+
+    /**
+     * Retrieve cube's size.
+     *
+     * @return int
+     */
+    public function getSize ()
+    {
+        return $this->size;
     }
 }
--- app/Cube/Parser.php
+++ app/Cube/Parser.php
@@ -49,17 +49,34 @@ class Parser
         foreach ($this->commands as $command) {
             $parts = explode(' ', $command);
             if ($this->remaining_queries == 0) {
-                // Expect a init command
+                // Expect an init command
                 if ($this->validate_init($parts)) {
                     $this->cube->init($parts[0]);
                     $this->remaining_queries = $parts[1];
                 } else {
                     $result['error_line'] = $line;
-                    $result['error'] = 'Wrong init line. Format is: "N M" with N < 100 and M < 1000';
+                    $result['error'] = 'Wrong init line. Format is "N M" with N < 100 and M < 1000.';
                     break;
                 }
             } else {
                 // Expect a query
+                switch ($parts[0]) {
+                    case 'UPDATE':
+                        if ($this->validate_update($parts)) {
+                            $this->cube->update($parts[1], $parts[2], $parts[3], $parts[4]);
+                        } else {
+                            $result['error_line'] = $line;
+                            $result['error'] = 'Wrong query. Format is "UPDATE X Y Z W". X, Y, Z cannot exceed N.';
+                            break 2;
+                        }
+                        break;
+                    case 'QUERY':
+                        break;
+                    default:
+                        $result['error_line'] = $line;
+                        $result['error'] = 'Unrecognized query.';
+                        break 2;
+                }
                 $this->remaining_queries--;
             }
             $line++;
@@ -79,9 +96,36 @@ class Parser
     {
         if (count($command) != 2) {
             return false;
-        } else if (!is_numeric($command[0]) || !is_numeric($command[0])) {
+        } else if (!is_numeric($command[0]) || !is_numeric($command[1])) {
             return false;
-        } else if ($command[0] > 100 || $command[1] > 1000) {
+        } else if ($command[0] > 100 || $command[0] < 1 || $command[1] > 1000 || $command[1] < 1) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Validate an update query.
+     *
+     * @param array    $query  query exploded into an array
+     *
+     * @return bool
+     */
+    public function validate_update ($query)
+    {
+        $size = $this->cube->getSize();
+        if (count($query) != 5) {
+            return false;
+        } else if (!is_numeric($query[1])) {
+            return false;
+        } else if (!is_numeric($query[2])) {
+            return false;
+        } else if (!is_numeric($query[3])) {
+            return false;
+        } else if (!is_numeric($query[4])) {
+            return false;
+        } else if ($query[1] > $size || $query[2] > $size || $query[3] > $size) {
             return false;
         }
 
